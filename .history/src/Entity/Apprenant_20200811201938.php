dc033b4763f3f32cd2962f29ba93c69fe84198a3
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2020-08-11 21:27:17 +0200

    add promotion

---
+++ .history/src/Entity/Apprenant_20200811201938.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ApprenantRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=ApprenantRepository::class)
+ * @ApiResource(
+ *  itemOperations={
+ *      "put","get"
+ *  },
+ *  normalizationContext={"groups"={"apprenant:read"}}
+ * )
+ */
+class Apprenant 
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"apprenant:read","promotion:read_all","promo_groupe_apprenants:read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=true)
+     * @Groups({"apprenant:read","promotion:read_all","promo_groupe_apprenants:read"})
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=ProfilSortie::class, inversedBy="apprenants")
+     * @Groups({"apprenant:read","promotion:read_all","promo_groupe_apprenants:read"})
+     */
+    private $profilSortie;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Statut::class, inversedBy="apprenants")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"apprenant:read","promotion:read_all","promo_groupe_apprenants:read"})
+     */
+    private $statut;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Groupe::class, mappedBy="apprenants")
+     */
+    private $groupes;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Promotion::class, inversedBy="apprenants")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $promotion;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $attente=true;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    public function __construct()
+    {
+        $this->groupes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getProfilSortie(): ?ProfilSortie
+    {
+        return $this->profilSortie;
+    }
+
+    public function setProfilSortie(?ProfilSortie $profilSortie): self
+    {
+        $this->profilSortie = $profilSortie;
+
+        return $this;
+    }
+
+    public function getStatut(): ?Statut
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?Statut $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Groupe[]
+     */
+    public function getGroupes(): Collection
+    {
+        return $this->groupes;
+    }
+
+    public function addGroupe(Groupe $groupe): self
+    {
+        if (!$this->groupes->contains($groupe)) {
+            $this->groupes[] = $groupe;
+            $groupe->addApprenant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGroupe(Groupe $groupe): self
+    {
+        if ($this->groupes->contains($groupe)) {
+            $this->groupes->removeElement($groupe);
+            $groupe->removeApprenant($this);
+        }
+
+        return $this;
+    }
+
+    public function getPromotion(): ?Promotion
+    {
+        return $this->promotion;
+    }
+
+    public function setPromotion(?Promotion $promotion): self
+    {
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+
+    public function getAttente(): ?bool
+    {
+        return $this->attente;
+    }
+
+    public function setAttente(bool $attente): self
+    {
+        $this->attente = $attente;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function sendEmail(\Swift_Mailer $mailer,$password){
+
+        $msg = (new \Swift_Message('Sonatel Academy'))
+        ->setFrom('dioufbadaraalioune7@gmail.com')
+        ->setTo($this->email)
+        ->setBody("Bonjour votre paswwor est : ")
+    }
+}
