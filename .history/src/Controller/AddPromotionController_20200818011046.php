ac5fe92c7d80479f8445fd2d227566a51e9b52cd
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2020-08-18 18:06:40 +0200

    fin gestion promotion et relance.

---
+++ .history/src/Controller/AddPromotionController_20200818011046.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Groupe;
+use App\Entity\Apprenant;
+use App\Entity\Promotion;
+use App\Repository\LangueRepository;
+use App\Repository\FabriqueRepository;
+use App\Repository\UserProfilRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use ApiPlatform\Core\Validator\ValidatorInterface;
+use App\Repository\FormateurRepository;
+use App\Repository\PromotionRepository;
+use App\Repository\ReferentielRepository;
+use App\Repository\StatutRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+// instantiation, when using it as a component
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+
+/**
+ * @Route("/api")
+ */
+class AddPromotionController extends AbstractController
+{
+    /**
+     * @Route("/admin/promotion", name="add_promotion", methods="POST")
+     */
+    public function addPromotion(
+        FormateurRepository $repoFormateur,
+        StatutRepository $repoStatus,
+        ReferentielRepository $reporef,
+        LangueRepository $repoLangue,
+        FabriqueRepository $repoFabrique,
+        SerializerInterface $serializer,
+        Request $request,
+        ValidatorInterface $validator,
+        EntityManagerInterface $em,
+        UserPasswordEncoderInterface $encoder,
+        \Swift_Mailer $mailer,
+        UserProfilRepository $repoProfil
+    ) {
+        $promotionTab = $request->request->all();
+        $promotion = $serializer->denormalize($promotionTab, Promotion::class, true, ["groups" => "promotion:write"]);
+
+        // Verification des dates -----------------------
+        $promotion->setDateDebut(new \DateTime());
+        if ($promotion->getDateDebut() > $promotion->getDateFin()) {
+            return new JsonResponse("La date de fin doit etre superieur a la date de debut.", Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        // Traitement Langue ----------------------------
+        if (empty($promotionTab['langue'])) {
+            return new JsonResponse("La langue est obligatoire", Response::HTTP_BAD_REQUEST, [], true);
+        }
+        $promotion->setLangue($repoLangue->findBy(array('libelle' => $promotionTab['langue']))[0]);
+
+        // Traitement Fabrique ----------------------
+        if (!isset($promotionTab['fabrique']) || empty($promotionTab['fabrique'])) {
+            return new JsonResponse("La langue est obligatoire", Response::HTTP_BAD_REQUEST, [], true);
+        }
+        $promotion->setFabrique($repoFabrique->findBy(array('libelle' => $promotionTab['fabrique']))[0]);
+
+        // Traitement Groupes ---------------------
+        $groupe = new Groupe();
+        $groupe->setLibelle("Groupe principal");
+        $groupe->setDateCreation(new \DateTime());
+        $promotion->addGroupe($groupe);
+
+        // Traitement referentiels --------------------
+        foreach ($promotionTab['referentiels'] as $value) {
+            if (!empty($value)) {
+                $referentiel = $reporef->findBy(array('libelle' => $value));
+                $promotion->addReferentiel($referentiel[0]);
+            }
+        }
+
+        // Traitement Image --------------------
+        $image = $request->files;
+        if (is_null($image->get('image'))) {
+            return new JsonResponse("L'image est obligatoire", Response::HTTP_BAD_REQUEST, [], true);
+        }
+        $imageType = explode("/", $image->get('image')->getMimeType())[1];
+        $imagePath = $image->get('image')->getRealPath();
+
+        $image = file_get_contents($imagePath, 'img/img.' . $imageType);
+        $promotion->setimage($image);
+
+        // Traitement Apprenants ---------------
+        $emailApprenat = array();
+        if (!empty($request->files->get('fichier'))) {
+            $filename = $request->files->get('fichier')->getRealPath();
+            $emailApprenatEx = $this->readFileExcel($filename);
+            foreach ($emailApprenatEx as $value) {
+                $emailApprenat[] = $value[0];
+            }
+        }
+        if (count($promotionTab['apprenants']) > 0) {
+            foreach ($promotionTab['apprenants'] as $value) {
+                if (!in_array($value, $emailApprenat)) {
+                    $emailApprenat[] = $value;
+                }
+            }
+        }
+        if (count($emailApprenat) < 1) {
+            return new JsonResponse("Les Apprenants sont obligatoire", Response::HTTP_BAD_REQUEST, [], true);
+        }
+        foreach ($emailApprenat as $value) {
+            if (!empty($value)) {
+                $apprenant = new Apprenant();
+                $user = new User();
+                $user->setEmail($value);
+                $password = "1234-" . $value[4] . $value[0] . $value[3];
+                $user->setPassword($encoder->encodePassword(new User(), $password));
+                $user->setUsername(explode("@", $value)[0]);
+                $user->setFirstname("firstname");
+                $user->setLastname("lastname");
+                $user->setProfil($repoProfil->findBy(['libelle' => "APPRENANT"])[0]);
+                $apprenant->setStatut($repoStatus->find(1));
+                $apprenant->setUser($user);
+                if ($promotion->addApprenant($apprenant)) {
+                    $groupe->addApprenant($apprenant);
+                    $user->sendEmail($mailer, $password);
+                }
+            }
+        }
+
+        // Traitement Formateur -----------------------
+        if (!isset($promotionTab['formateurs']) || empty($promotionTab['formateurs'])) {
+            return new JsonResponse("Les formateurs sont obligatoire", Response::HTTP_BAD_REQUEST, [], true);
+        }
+        foreach ($promotionTab['formateurs'] as $value) {
+            if (!empty($value)) {
+                $formateurs = $repoFormateur->find($value);
+                $promotion->addFormateur($formateurs);
+            }
+        }
+
+        // validation du promotion ----------------------------
+        $errors = $validator->validate($promotion);
+        if (($errors) > 0) {
+            $errorsString = $serializer->serialize($errors, 'json');
+            return new JsonResponse($errorsString, Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        // L'insertion du promotion -----------------------------
+        $em->persist($promotion);
+        $em->persist($promotion);
+        $em->flush();
+        return new JsonResponse('success', Response::HTTP_CREATED, [], true);
+    }
+
+
+    /**
+     * @Route("/admin/promotion/{id_promo}/relanceOne/{id_apprenant}", name="relance_groupes", methods="POST")
+     */
+    public function relanceAll(int $id_promo,int $id_apprenant,PromotionRepository $repoPromo){
+        $promo =  $repoPromo->find($id_promo);
+
+        $
+    }
+
+    /**
+     * @Route("/admin/promotion/{id_promo}/relanceAll", name="relance_individuel", methods="POST")
+     */
+    public function relanceOne(){
+
+    }
+
+    public function readFileExcel($filename)
+    {
+        $reader = \PHPExcel_IOFactory::createReaderForFile($filename);
+        $reader->setReadDataOnly(true);
+        $wb = $reader->load($filename);
+        $ws = $wb->getSheet(0);
+        $rows = $ws->toArray();
+        return $rows;
+    }
+}
---
+++ .history/src/Entity/Promotion_20200818004121.php
@@ -0,0 +1,449 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PromotionRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=PromotionRepository::class)
+ * @UniqueEntity(
+ * fields={"titre"},
+ * message="Le titre doit être unique."
+ * )
+ * @ApiResource(
+ *  routePrefix="/admin",
+ *  attributes={"security"="is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR')"},
+ *  collectionOperations={
+ *      "get"={
+ *          "normalization_context"={"groups"={"promotion:read"}},
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))"
+ *      },
+ *     "get_promo_principal"={
+ *          "method"="GET",
+ *          "path"="/promotions/principal",
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))",
+ *          "normalization_context"={"groups"={"promotion:read_all"}}
+ *      },
+ *      "get_apprenants_attente"={
+ *         "method"="GET",
+ *         "path"="/promotions/apprenants/attente",
+ *         "controller"=ShowApprenantsAttente::class,
+ *         "route_name"="show_apprenants_attente"
+ *     },
+ *     "add_promotion" = {
+ *          "method"="POST",
+ *          "path"="/promotion",
+ *          "route_name"="add_promotion"
+ *      }
+ *  },
+ *  itemOperations={
+ *      "get"={
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))"
+ *      }, 
+ *      "get_promo_principal_id"={
+ *          "method"="GET",
+ *          "path"="/promotions/{id}/principal",
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))",
+ *          "normalization_context"={"groups"={"promotion:read_all"}}
+ *      },  
+ *      "get_promo_id_referentiel"={
+ *          "method"="GET",
+ *          "path"="/promotions/{id}/referentiels",
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))",
+ *          "normalization_context"={"groups"={"promotion:read_all_ref"}}
+ *      },    
+ *      "get_apprenants_id_attente"={
+ *         "method"="GET",
+ *         "path"="/promotions/{id}/apprenants/attente",
+ *         "controller"=ShowApprenantsAttenteById::class,
+ *         "route_name"="show_apprenants_id_attente",
+ *         "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))"
+ *     },
+ *      "get_promo_id_groupes_id_apprenants"={
+ *          "method"="GET",
+ *          "path"="/promotions/{id_promo}/groupes/{id_groupe}/apprenants",
+ *          "requirements"={"id_promo"="\d+"},
+ *          "controller"=ShowApprenantsByGroupeAndPromo::class,
+ *          "route_name"="show_promo_id_groupes_id_apprenants",
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))",
+ *      },
+ *      "get_promo_id_formateurs"={
+ *          "method"="GET",
+ *          "path"="/promotions/{id}/formateurs",
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))",
+ *      },
+ *      "put_promotion_referentiles" = {
+ *          "method"="PUT",
+ *          "path"="/promotion/{id}/referentiels",
+ *          "route_name"="edit_promotion_referentiles"
+ *      },
+ *      "put_promotion_apprenants" = {
+ *          "method"="PUT",
+ *          "path"="/promotion/{id}/apprenants",
+ *          "route_name"="edit_promotion_apprenants",
+ *          "normalization_context"={"groups"={"promotion:read_all"}}
+ *      },
+ *      "put_promotion_formateurs" = {
+ *          "method"="PUT",
+ *          "path"="/promotion/{id}/formateurs",
+ *          "route_name"="edit_promotion_formateurs"
+ *      },
+ *      "put_promotion_groupes" = {
+ *          "method"="PUT",
+ *          "path"="/promotion/{id_promo}/groupes/{id_groupe}",
+ *          "route_name"="edit_promotion_groupes"
+ *      },
+ *      "post_promotion_relance_groupes" = {
+ *          "method"="POST",
+ *          "path"="/promotion/{id_promo}/apprenants",
+ *          "route_name"="edit_promotion_groupes"
+ *      },
+ *      "post_promotion_relance_groupes" = {
+ *          "method"="PUT",
+ *          "path"="/promotion/{id_promo}/groupes/{id_groupe}",
+ *          "route_name"="edit_promotion_groupes"
+ *      } 
+ *  }
+ * )
+ */
+class Promotion
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"groupe:read","promotion:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"groupe:read","promotion:write","promotion:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     * @Assert\NotBlank(message="Titre obligatoire.")
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"groupe:read","promotion:read","promotion:write","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     * @Assert\NotBlank(message="Descrition obligatoire.")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"groupe:read","promotion:write","promotion:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     * @Assert\NotBlank(message="Le lieu est obligatoire.")
+     */
+    private $lieu;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"groupe:read","promotion:write","promotion:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     * @Assert\NotBlank(message="La reference agate est obligatoire.")
+     */
+    private $referenceAgate;
+
+    /**
+     * @ORM\Column(type="date")
+     * @Groups({"groupe:read","promotion:write","promotion:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     * @Assert\NotBlank(message="Date de debut obligatoire.")
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="date")
+     * @Groups({"groupe:read","promotion:write","promotion:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     * @Assert\NotBlank(message="Date de fin obligatoire.")
+     */
+    private $dateFin;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Referentiel::class, inversedBy="promotions")
+     * @Groups({"promotion:read","groupe:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     */
+    private $referentiels;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     * @Groups({"groupe:read","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     */
+    private $image;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Langue::class, inversedBy="promotions")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"groupe:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     */
+    private $langue;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Fabrique::class, inversedBy="promotions")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"groupe:read","promotion:read_all","promotion:read_all_ref","promo_groupe_apprenants:read"})
+     */
+    private $fabrique;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Groupe::class, mappedBy="promotion", cascade={"persist"})
+     * @Groups({"promotion:read","promotion:read_all","promo_groupe_apprenants:read"})
+     */
+    private $groupes;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Apprenant::class, mappedBy="promotion", cascade={"persist"})
+     * @Groups({""})
+     */
+    private $apprenants;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Formateur::class, mappedBy="promotions")
+     * @Groups({"promotion:read","promotion:read_all"})
+     */
+    private $formateurs;
+
+    public function __construct()
+    {
+        $this->referentiels = new ArrayCollection();
+        $this->groupes = new ArrayCollection();
+        $this->apprenants = new ArrayCollection();
+        $this->formateurs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(?string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getReferenceAgate(): ?string
+    {
+        return $this->referenceAgate;
+    }
+
+    public function setReferenceAgate(?string $referenceAgate): self
+    {
+        $this->referenceAgate = $referenceAgate;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Referentiel[]
+     */
+    public function getReferentiels(): Collection
+    {
+        return $this->referentiels;
+    }
+
+    public function addReferentiel(Referentiel $referentiel): self
+    {
+        if (!$this->referentiels->contains($referentiel)) {
+            $this->referentiels[] = $referentiel;
+        }
+
+        return $this;
+    }
+
+    public function removeReferentiel(Referentiel $referentiel): self
+    {
+        if ($this->referentiels->contains($referentiel)) {
+            $this->referentiels->removeElement($referentiel);
+        }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        
+        return $this->image!=null?stream_get_contents($this->image):null;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = base64_encode($image);
+
+        return $this;
+    }
+
+    public function getLangue(): ?Langue
+    {
+        return $this->langue;
+    }
+
+    public function setLangue(?Langue $langue): self
+    {
+        $this->langue = $langue;
+
+        return $this;
+    }
+
+    public function getFabrique(): ?Fabrique
+    {
+        return $this->fabrique;
+    }
+
+    public function setFabrique(?Fabrique $fabrique): self
+    {
+        $this->fabrique = $fabrique;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Groupe[]
+     */
+    public function getGroupes(): Collection
+    {
+        return $this->groupes;
+    }
+
+    public function addGroupe(Groupe $groupe): self
+    {
+        if (!$this->groupes->contains($groupe)) {
+            $this->groupes[] = $groupe;
+            $groupe->setPromotion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGroupe(Groupe $groupe): self
+    {
+        if ($this->groupes->contains($groupe)) {
+            $this->groupes->removeElement($groupe);
+            // set the owning side to null (unless already changed)
+            if ($groupe->getPromotion() === $this) {
+                $groupe->setPromotion(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Apprenant[]
+     */
+    public function getApprenants(): Collection
+    {
+        return $this->apprenants;
+    }
+
+    public function addApprenant(Apprenant $apprenant): self
+    {
+        if (!$this->apprenants->contains($apprenant)) {
+            $this->apprenants[] = $apprenant;
+            $apprenant->setPromotion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeApprenant(Apprenant $apprenant): self
+    {
+        if ($this->apprenants->contains($apprenant)) {
+            $this->apprenants->removeElement($apprenant);
+            // set the owning side to null (unless already changed)
+            if ($apprenant->getPromotion() === $this) {
+                $apprenant->setPromotion(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Formateur[]
+     */
+    public function getFormateurs(): Collection
+    {
+        return $this->formateurs;
+    }
+
+    public function addFormateur(Formateur $formateur): self
+    {
+        if (!$this->formateurs->contains($formateur)) {
+            $this->formateurs[] = $formateur;
+            $formateur->addPromotion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFormateur(Formateur $formateur): self
+    {
+        if ($this->formateurs->contains($formateur)) {
+            $this->formateurs->removeElement($formateur);
+            $formateur->removePromotion($this);
+        }
+
+        return $this;
+    }
+}
