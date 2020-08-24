773f0a15fc062c30bc4747371d82b6bd5a11847d
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2020-08-25 00:58:33 +0200

    gestion route get brief.

---
+++ .history/src/Entity/EtatBriefGroupe_20200825000804.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\EtatBriefGroupeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=EtatBriefGroupeRepository::class)
+ * @ApiResource()
+ */
+class EtatBriefGroupe
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"briefGroupe:read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=StatutBrief::class, inversedBy="etatBriefGroupes")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"briefGroupe:read"})
+     */
+    private $statut;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Brief::class, mappedBy="etatBriefGroupe")
+     * @Groups({"briefGroupe:read"})
+     */
+    private $brief;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Groupe::class, mappedBy="etatBriefGroupe")
+     * 
+     */
+    private $groupe;
+
+    public function __construct()
+    {
+        $this->brief = new ArrayCollection();
+        $this->groupe = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatut(): ?StatutBrief
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?StatutBrief $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Brief[]
+     */
+    public function getBrief(): Collection
+    {
+        return $this->brief;
+    }
+
+    public function addBrief(Brief $brief): self
+    {
+        if (!$this->brief->contains($brief)) {
+            $this->brief[] = $brief;
+            $brief->setEtatBriefGroupe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBrief(Brief $brief): self
+    {
+        if ($this->brief->contains($brief)) {
+            $this->brief->removeElement($brief);
+            // set the owning side to null (unless already changed)
+            if ($brief->getEtatBriefGroupe() === $this) {
+                $brief->setEtatBriefGroupe(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Groupe[]
+     */
+    public function getGroupe(): Collection
+    {
+        return $this->groupe;
+    }
+
+    public function addGroupe(Groupe $groupe): self
+    {
+        if (!$this->groupe->contains($groupe)) {
+            $this->groupe[] = $groupe;
+            $groupe->setEtatBriefGroupe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGroupe(Groupe $groupe): self
+    {
+        if ($this->groupe->contains($groupe)) {
+            $this->groupe->removeElement($groupe);
+            // set the owning side to null (unless already changed)
+            if ($groupe->getEtatBriefGroupe() === $this) {
+                $groupe->setEtatBriefGroupe(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ .history/src/Entity/Ressource_20200824231127.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\RessourceRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=RessourceRepository::class)
+ * @ApiResource()
+ */
+class Ressource
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"brief_livrable_partiel:read","brief:read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"brief_livrable_partiel:read","brief:read"})
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     * @Groups({"brief_livrable_partiel:read","brief:read"})
+     */
+    private $pieceJointe;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Brief::class, inversedBy="ressource")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $brief;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getPieceJointe()
+    {
+        return $this->pieceJointe;
+    }
+
+    public function setPieceJointe($pieceJointe): self
+    {
+        $this->pieceJointe = $pieceJointe;
+        
+
+        return $this;
+    }
+
+    public function getBrief(): ?Brief
+    {
+        return $this->brief;
+    }
+
+    public function setBrief(?Brief $brief): self
+    {
+        $this->brief = $brief;
+
+        return $this;
+    }
+}
